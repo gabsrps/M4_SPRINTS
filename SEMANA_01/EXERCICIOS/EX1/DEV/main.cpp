6fe23c843fa25199b73cc28cc8652dfe6023b675
gabsrps <gabriel.santos@sou.inteli.edu.br>
2022-10-26 14:11:45 -0300

    Exercícios semana 1

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -1,87 +1,169 @@
+// Ex 1:
+/*
 #include <iostream>
-#include <string>
+using namespace std;
 
-// 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente 
-// entre dois valores mínimo e máximo e retorna esse valor
+int main() {
+  int num1 = 0, num2 = 0, num3 = 0;
 
-// 2 - Faça uma função que simule a leitura de um sensor lendo o 
-// valor do teclado ao final a função retorna este valor
+  cout << "Escreva o número de medida: ";
+  cin >> num1;
+  cout << "Escreva o número máximo: ";
+  cin >> num2;
+  cout << "Escreva o número mínimo: ";
+  cin >> num3;
 
-// 3 - Faça uma função que armazena uma medida inteira qualquer 
-// em um vetor fornecido. Note que como C não possui vetores 
-// nativos da linguagem, lembre-se que você precisa passar o 
-// valor máximo do vetor assim como a última posição preenchida
-// Evite também que, por acidente, um valor seja escrito em 
-// uma área de memória fora do vetor
+  int sub1 = 0, sub2 = 0;
+  sub1 = num1 - num3;
+  cout << "Subtração da medida pelo mínimo: " << sub1 << endl;
 
+  sub2 = num2 - num3;
+  cout << "Subtração do máximo pelo mínimo: " << sub2 << endl;
 
-
-// 4 - Faça uma função que recebe um vetor com 4 posições que contém 
-// o valor da distância de um pequeno robô até cada um dos seus 4 lados.
-// A função deve retornar duas informações: A primeira é a direção 
-// de maior distância ("Direita", "Esquerda", "Frente", "Tras") e a 
-// segunda é esta maior distância.
-
-
-
-
-// 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e 
-// retorna verdadeiro ou falso
-
-
-// 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô 
-// e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", 
-// "Frente", "Tras"). 
-//      A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno 
-// para uma movimentação futura. 
-//      Ele vai armazenando estas distancias em um vetor fornecido como parâmetro 
-// e retorna a ultima posicao preenchida do vetor.
-//      Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado 
-//
-//      Para simular os sensores e os comandos de pare, use as funções já construídas 
-// nos ítens anteriores e em um looping contínuo até que um pedido de parada seja 
-// enviado pelo usuário. 
-//
-//      Complete a função com a chamada das funções já criadas
-int dirige(int *v,int maxv){
-	int maxVetor = maxv;
-	int *vetorMov = v;
-	int posAtualVetor = 0;
-	int dirigindo = 1;		
-	while(dirigindo){		
-		int medida = /// .. Chame a função de de leitura da medida para a "Direita"
-		medida = converteSensor(medida,0,830);
-		posAtualVetor = // Chame a função para armazenar a medida no vetor
-        ///////////////////////////////////////////////////////////////////////////		
-		// Repita as chamadas acima para a "Esquerda", "Frente", "Tras"
-		// ................
-		///////////////////////////////////////////////////////////////////////////
-		dirigindo = leComando();		
-	}
-	return posAtualVetor;
+  if (num1 > num2) {
+    cout << "A medida é maior que o valor máximo! ";
+  } else if (num1 < num3) {
+    cout << "A medida é menor que o valor mínimo! ";
+  } else {
+    cout << num1 << " vale ";
+  }
+  float res = 0;
+  res = sub1 * 100 / sub2;
+  cout << res << "% do intervalo entre " << num3 << " e " << num2;
 }
+*/
 
+// Ex 2:
+/*
+#include <iostream>
 
-// O trecho abaixo irá utilizar as funções acima para ler os sensores e o movimento
-// do robô e no final percorrer o vetor e mostrar o movimento a cada direção baseado 
-// na maior distância a cada movimento
-void percorre(int *v,int tamPercorrido){		
-	int *vetorMov = v;
-	int maiorDir = 0;
-	
-	for(int i = 0; i< tamPercorrido; i+=4){
-		char *direcao = direcaoMenorCaminho(&(vetorMov[i]),&maiorDir);
-		printf("Movimentando para %s distancia = %i\n",direcao,maiorDir);
-	}
+using namespace std;
+
+int main() {
+  int num;
+      cout << "Digite um número:" <<endl;
+  cin >> num;
+
+  cout << "Seu número é: " << num <<endl;
 }
+*/
 
-int main(int argc, char** argv) {
-	int maxVetor = 100;
-	int vetorMov[maxVetor*4];
-	int posAtualVet = 0;
-	
-	posAtualVet = dirige(vetorMov,maxVetor);
-	percorre(vetorMov,posAtualVet);
-	
-	return 0;
+// Ex3:
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+int num = 0;
+  vector<int> vetor {1, 2, 3, 4, 5};
+
+  cout << "\nVetor inicial: ";
+
+  for (const int& i : vetor) {
+    cout << i << "  ";
+  }
+
+cout << "\nDigite um número para adicionar ao vetor: " <<endl;
+cin >> num;
+
+  vetor.push_back(num);
+
+  cout << "\nO tamanho do vetor é: " << vetor.size() <<endl;
+
+
+  cout << "\nVetor atualizado: ";
+  for (const int& i : vetor) {
+    cout << i << "  ";
+
+  }
+
+  return 0;
 }
+*/
+
+// Ex 4:
+/*
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+int num = 0, direita = 0, esquerda = 0, frente = 0, tras = 0, maior_val = 0;
+  vector<int> vetor {};
+
+  cout << "\nInsira o valor de direita: ";
+  cin >> direita;
+  vetor.push_back(direita);
+
+  cout << "\nInsira o valor de esquerda: ";
+  cin >> esquerda;
+  vetor.push_back(esquerda);
+
+  cout << "\nInsira o valor de frente: ";
+  cin >> frente;
+  vetor.push_back(frente);
+
+  cout << "\nInsira o valor de trás: ";
+  cin >> tras;
+  vetor.push_back(tras);
+
+  cout << "\nO vetor é esse: ";
+  for (const int& i : vetor) {
+    cout << i << "  ";
+    }
+    maior_val = *max_element(vetor.begin(), vetor.end());
+    cout << "\nValor máximo: " << maior_val <<endl;
+
+  if (direita == maior_val){
+    cout << "direita";
+    }else if (esquerda == maior_val){
+    cout << "esquerda";
+    }else if (frente == maior_val){
+    cout << "frente";
+    }else if (tras == maior_val){
+    cout << "trás";
+  }
+}
+*/
+
+// Ex 5:
+/*
+#include <iostream>
+using namespace std;
+int main() {
+int val = 1;
+  while (val != 0){
+    cout << "Digite 0 ou 1 " <<endl;
+    cin >> val;
+    while (val != 0) {
+      cout << "Acesso negado! Tente digitar outra vez: " <<endl;
+      cin >> val;
+  if (val != 1) {
+    cout << "Acesso concedido!";
+}}}}
+*/
+
+// Ex 6:
+/*
+#include <bits/stdc++.h>
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+int main() {
+int num = 0, direita = 0, esquerda = 0, frente = 0, tras = 0;
+  cout << "\nDigite o valor de direita: ";
+  cin >> direita;
+  cout << "\nDigite o valor de esquerda: ";
+  cin >> esquerda;
+  cout << "\nDigite o valor de frente: ";
+  cin >> frente;
+  cout << "\nDigite o valor de trás: ";
+  cin >> tras;
+
+  cout << "{Direita: " << direita << "; Esquerda: " << esquerda << "; Frente: "
+<< frente << "; Trás: " << tras << "}";
+
+  }
+*/
